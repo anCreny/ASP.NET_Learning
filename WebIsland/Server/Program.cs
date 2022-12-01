22d569c69f3425fad538da1bc04cd733710f1efb
anCreny <artem.0110@mail.ru>
2022-12-01 17:29:02 +0300

    Testing cookies

--- WebIsland/Server/Program.cs
+++ WebIsland/Server/Program.cs
@@ -1,12 +1,43 @@
 var builder = WebApplication.CreateBuilder();
+
 var app = builder.Build();
 
-app.Run(async (c) => await c.Response.SendFileAsync("HTML/index.html"));
+app.Run(async (c) =>
+{
+    if (c.Request.Path == "/api")
+    {
+        var number = 0;
+        try
+        {
+            Number? value = await c.Request.ReadFromJsonAsync<Number>();
+            if (value is not null)
+            {
+                number = value.Value;
+            }
+        }
+        catch
+        {
+        }
+        
+        c.Response.Cookies.Append("value", number.ToString());
+    }
+    else
+    {
+        if (c.Request.Cookies.TryGetValue("value", out var number))
+        {
+            await c.Response.WriteAsync(number);
+        }
+        else
+        {
+            await c.Response.SendFileAsync("HTML/index.html");
+        }
+    }
+});
 
 app.Run();
 
 
-
+public record Number(int Value);
 
 
 
